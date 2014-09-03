0dcbac9a974a7e23b47a974165350fb9ee5cdb85
isaacabensur <isaacabensur@gmail.com>
2014-09-03 17:00:58 -0500

    Capa Negocio Ckeckin

---
+++ Negocio/UPC.CruzDelSur.Negocio.Modelo.CheckIn/BE_Equipaje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UPC.CruzDelSur.Negocio.Modelo.CheckIn
+{
+    public class BE_Equipaje
+    {
+
+        public string NroBoleto { get; set; }
+        public string Pasajero { get; set; }
+        public string Peso { get; set; }
+        public string Origen { get; set; }
+        public string Destino { get; set; }
+        public string FechaSalida { get; set; }
+        public string HoraSalida { get; set; }
+        public string TipoEquipaje { get; set; }
+        public string Ubicacion { get; set; }
+        public string FechaActual { get; set; }
+        public string HoraActual { get; set; }
+        public string EstadoEquipaje { get; set; }
+        public int CodBoleto { get; set; }
+    }
+}
---
+++ Negocio/UPC.CruzDelSur.Negocio.Modelo.CheckIn/BE_Tiket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPC.CruzDelSur.Negocio.Modelo.CheckIn
+{
+    public class BE_Tiket
+    {
+
+        public string Codigo { get; set; }
+        public string Numero { get; set; }
+        public string Peso { get; set; }
+        public string Tamano { get; set; }
+        public string CodBarra { get; set; }
+        public string EstadoEquipaje { get; set; }
+        public string TipoEtiqueta { get; set; }
+        public int CodBoleto { get; set; }
+    }
+}
